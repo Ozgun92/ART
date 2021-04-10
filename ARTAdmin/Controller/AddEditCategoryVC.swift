bdfb78d8eaf707d9b43a7f26c4d256628662a8d3
Özgün Yildiz <ozgun.yildiz@hotmail.de>
2021-04-10 12:23:47 +0200

    Storyboard changes

--- ARTAdmin/Controller/AddEditCategoryVC.swift
+++ ARTAdmin/Controller/AddEditCategoryVC.swift
@@ -49,7 +49,7 @@ class AddEditCategoryVC: UIViewController {
     @objc func imgTapped(_ tap: UITapGestureRecognizer) {
         launchImgPicker()
     }
-
+    
     @IBAction func addCategoryClicked(_ sender: Any) {
         activityIndicator.startAnimating()
         uploadImageThenDocument()
@@ -58,71 +58,76 @@ class AddEditCategoryVC: UIViewController {
     
     
     func uploadImageThenDocument() {
-
         guard let image = categoryImg.image, let categoryName = nameText.text, categoryName.isNotEmpty else { simpleAlert(title: "Error", message: "Must add category Image and name")
             return
         }
-
+        
         // Step 1: Turn the image into Data
-
+        
         guard let imageData = image.jpegData(compressionQuality: 0.2) else { return }
-
+        
         // Step 2: Create a storage image reference -> A location in Firestorage for it to be stored
-
+        
         let imageRef = Storage.storage().reference().child("/categoryImages/\(categoryName).jpg")
-
-
+        
+        
         // Step 3: Set the meta data
-
+        
         let metaData = StorageMetadata()
         metaData.contentType = "image/jpg"
-
+        
         // Step 4: Upload the data
-
+        
         imageRef.putData(imageData, metadata: metaData) { (metaData, error) in
             if let error = error {
                 self.handleError(error: error, message: "Unable to upload image")
                 return
             }
-
+            
             // Step 5: Once the image is uploaded, retrieve the download URL
-
+            
             imageRef.downloadURL { (url, error) in
                 if let error = error {
                     self.handleError(error: error, message: "Unable to retrieve image url.")
                     return
-            }
-
+                }
+                
                 guard let url = url else { return }
                 print(url)
-
+                
                 // Step 6: Upload new Category document in the Firestore categories collection
                 self.uploadDocument(url: url.absoluteString)
             }
         }
     }
-
+    
     func uploadDocument(url: String) {
-
+        
         // we are doing it like this because we have to take into account whether we are modifying or creating a category
         var docRef: DocumentReference!
         var category = Category(name: nameText.text!, id: "", imgUrl: url, timeStamp: Timestamp())
-        docRef = Firestore.firestore().collection("categories").document()
-        category.id = docRef.documentID
-
+        
+        if let categoryToEdit = categoryToEdit {
+            // we are editing
+            docRef = Firestore.firestore().collection("categories").document(categoryToEdit.id)
+            category.id = categoryToEdit.id
+        } else {
+            // new category
+            docRef = Firestore.firestore().collection("categories").document()
+            category.id = docRef.documentID
+        }
+  
         let data = Category.modelToData(category: category)
         docRef.setData(data, merge: true) { (error) in
             if let error = error {
                 self.handleError(error: error, message: "Unable to upload new category to Firestore")
                 return
-        }
+            }
             self.navigationController?.popViewController(animated: true)
+        }
+   
     }
-}
 
-
-        
-        
     func handleError(error: Error, message: String) {
         debugPrint(error.localizedDescription)
         self.simpleAlert(title: "Error", message: message)
